e282cb31fa9a947e4d85655262b5a5c5b7c99933
Zerotonine <52973359+Zerotonine@users.noreply.github.com>
2020-10-27 11:18:41 +0100

    init

---
+++ CSH1_Tag1_Aufgabe_02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSH1_Tag1_Aufgabe_02
+{
+    class Program
+    {
+        static ushort i = 5;
+        static float f = 1.1f;
+        static char c = 'x';
+
+        static void Ausgabe()
+        {
+            string s = "";
+
+            for(int j = 0; j < i; j++)
+            {
+                s += c.ToString() + f;
+            }
+            for (int j = 0; j < 3; j++)
+            {
+                Console.WriteLine(s);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Ausgabe();
+            Console.ReadKey();
+        }
+    }
+}
